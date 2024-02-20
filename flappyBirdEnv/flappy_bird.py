4b4b7c51d0984fd53805b5b14287c5ccadbc5a31
Jathin Narayan <jathin04official@gmail.com>
2024-02-20 20:53:34 +0530

    v2.4

--- flappyBirdEnv/flappy_bird.py
+++ flappyBirdEnv/flappy_bird.py
@@ -170,9 +170,10 @@ def draw_window(win, bird, pipes, base):
 def main():
     bird = Bird(230, 350)
     base = Base(730)
-    pipes = [Pipe(700)]
+    pipes = [Pipe(700)] 
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
+    score = 0
 
     run = True
     while run:
@@ -182,6 +183,29 @@ def main():
                 run = False
         
         #bird.move()
+        add_pipe = False
+        rem = []
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass
+
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(700))
+
+        for r in rem:
+            pipes.remove(r)
+
+        base.move()
         draw_window(win, bird, pipes, base)
 
     pygame.quit()
