8e1658d26306008900b3f80d0f73fab2cf399e4a
Jathin Narayan <jathin04official@gmail.com>
2024-02-21 12:59:09 +0530

    v3, Birds flying

--- flappyBirdEnv/flappy_exp.py
+++ flappyBirdEnv/flappy_exp.py
@@ -2,7 +2,6 @@ import pygame
 import time
 import os 
 import random
-import neat
 
 pygame.font.init()
 
@@ -160,7 +159,7 @@ class Base:
         win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win, birds, pipes, base, score):
+def draw_window(win, bird, pipes, base, score):
     win.blit(BG_IMG, (0, 0))
     for pipe in pipes:
        pipe.draw(win)
@@ -169,28 +168,17 @@ def draw_window(win, birds, pipes, base, score):
     win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
 
     base.draw(win) 
-    for bird in birds:
-        bird.draw(win)
+
+    bird.draw(win)
     pygame.display.update()
 
 
-def main(genomes, config):
-    nets = []
-    ge = []
-    birds = []
-
-    for _, g in genomes:
-        net = neat.nn.FeedForwardNetwork.create(g, config)
-        nets.append(net)
-        birds.append(Bird(230, 350))
-        g.fitness = 0
-        ge.append(g)
-
+def main():
+    bird = Bird(230, 350)
     base = Base(730)
     pipes = [Pipe(600)] 
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
-    
     score = 0
 
     run = True
@@ -199,79 +187,41 @@ def main(genomes, config):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                pygame.quit()
-                quit()
-        
-        pipe_ind = 0
-        if len(birds) > 0:
-            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
-                pipe_ind = 1
-        else:
-            run = False
-            break
 
-        for x, bird in enumerate(birds):
-            bird.move()
-            ge[x].fitness += 0.1
-
-            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
-
-            if output[0] > 0.5:
-                bird.jump()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    bird.jump()
 
         add_pipe = False
         rem = []
         for pipe in pipes:
-            for x, bird in enumerate(birds):
-                if pipe.collide(bird):
-                    ge[x].fitness -= 1
-                    birds.pop(x)
-                    nets.pop(x)
-                    ge.pop(x)
+            if pipe.collide(bird):
+                pass
 
-                if not pipe.passed and pipe.x < bird.x:
-                    pipe.passed = True
-                    add_pipe = True
-            
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
-                    rem.append(pipe)
+                rem.append(pipe)
+
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
 
             pipe.move()
 
         if add_pipe:
             score += 1
-            for g in ge:
-                g.fitness += 5
             pipes.append(Pipe(600))
 
         for r in rem:
             pipes.remove(r)
 
-        for x, bird in enumerate(birds):
-            if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
-                birds.pop(x)
-                nets.pop(x)
-                ge.pop(x)
+        if bird.y + bird.img.get_height() >= 730:
+            pass
 
         base.move()
-        draw_window(win, birds, pipes, base, score)
+        bird.move()
+        draw_window(win, bird, pipes, base, score)
 
+    pygame.quit()
+    quit()
 
-def run(config_path):
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                                config_path)        #neat.config.Config
-    
-    p = neat.Population(config)
-    
-    p.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    p.add_reporter(stats)
-
-    winner = p.run(main, 50)
-
-    
-if __name__ == "__main__":
-    locall_dir = os.path.dirname(__file__)
-    config_path = os.path.join(locall_dir, "config_feedforward_68c3f3d4a7.txt")
-    run(config_path)
+main()
